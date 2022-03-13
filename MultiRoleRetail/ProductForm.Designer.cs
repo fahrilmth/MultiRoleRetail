593413bb22d505707fb3f7d02a988c77150af9c1
Fahril <fahril16093@gmail.com>
2022-03-13 16:09:11 +0700

    add product

---
+++ MultiRoleRetail/ProductForm.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace MultiRoleRetail
+{
+    partial class ProductForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgProduct = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.tbSearch = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.cbSupplierID = new System.Windows.Forms.ComboBox();
+            this.tbStock = new System.Windows.Forms.TextBox();
+            this.tbPrice = new System.Windows.Forms.TextBox();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbID = new System.Windows.Forms.TextBox();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgProduct)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgProduct
+            // 
+            this.dgProduct.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgProduct.Location = new System.Drawing.Point(12, 48);
+            this.dgProduct.Name = "dgProduct";
+            this.dgProduct.RowHeadersWidth = 62;
+            this.dgProduct.RowTemplate.Height = 33;
+            this.dgProduct.Size = new System.Drawing.Size(985, 495);
+            this.dgProduct.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 549);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(112, 34);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "NEW";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // tbSearch
+            // 
+            this.tbSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbSearch.Location = new System.Drawing.Point(12, 12);
+            this.tbSearch.Name = "tbSearch";
+            this.tbSearch.PlaceholderText = "  Search here";
+            this.tbSearch.Size = new System.Drawing.Size(985, 31);
+            this.tbSearch.TabIndex = 2;
+            this.tbSearch.TextChanged += new System.EventHandler(this.tbSearch_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnCancel);
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Controls.Add(this.cbSupplierID);
+            this.groupBox1.Controls.Add(this.tbStock);
+            this.groupBox1.Controls.Add(this.tbPrice);
+            this.groupBox1.Controls.Add(this.tbName);
+            this.groupBox1.Controls.Add(this.tbID);
+            this.groupBox1.Location = new System.Drawing.Point(1003, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(300, 268);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "PRODUCT";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(154, 223);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(140, 34);
+            this.btnCancel.TabIndex = 9;
+            this.btnCancel.Text = "CANCEL";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(6, 223);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(142, 34);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "SAVE";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // cbSupplierID
+            // 
+            this.cbSupplierID.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbSupplierID.FormattingEnabled = true;
+            this.cbSupplierID.Location = new System.Drawing.Point(6, 184);
+            this.cbSupplierID.Name = "cbSupplierID";
+            this.cbSupplierID.Size = new System.Drawing.Size(288, 33);
+            this.cbSupplierID.TabIndex = 8;
+            // 
+            // tbStock
+            // 
+            this.tbStock.Location = new System.Drawing.Point(6, 147);
+            this.tbStock.Name = "tbStock";
+            this.tbStock.PlaceholderText = "Stock";
+            this.tbStock.Size = new System.Drawing.Size(288, 31);
+            this.tbStock.TabIndex = 7;
+            // 
+            // tbPrice
+            // 
+            this.tbPrice.Location = new System.Drawing.Point(6, 110);
+            this.tbPrice.Name = "tbPrice";
+            this.tbPrice.PlaceholderText = "Price";
+            this.tbPrice.Size = new System.Drawing.Size(288, 31);
+            this.tbPrice.TabIndex = 6;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(6, 73);
+            this.tbName.Name = "tbName";
+            this.tbName.PlaceholderText = "Name";
+            this.tbName.Size = new System.Drawing.Size(288, 31);
+            this.tbName.TabIndex = 5;
+            // 
+            // tbID
+            // 
+            this.tbID.Location = new System.Drawing.Point(6, 36);
+            this.tbID.Name = "tbID";
+            this.tbID.PlaceholderText = "ID";
+            this.tbID.ReadOnly = true;
+            this.tbID.Size = new System.Drawing.Size(288, 31);
+            this.tbID.TabIndex = 4;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(130, 549);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(112, 34);
+            this.btnEdit.TabIndex = 4;
+            this.btnEdit.Text = "EDIT";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(248, 549);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(112, 34);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "DELETE";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // ProductForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1315, 595);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tbSearch);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dgProduct);
+            this.Name = "ProductForm";
+            this.Text = "Product";
+            this.Load += new System.EventHandler(this.Product_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgProduct)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DataGridView dgProduct;
+        private Button btnAdd;
+        private TextBox tbSearch;
+        private GroupBox groupBox1;
+        private Button btnCancel;
+        private Button btnSave;
+        private ComboBox cbSupplierID;
+        private TextBox tbStock;
+        private TextBox tbPrice;
+        private TextBox tbName;
+        private TextBox tbID;
+        private Button btnEdit;
+        private Button btnDelete;
+    }
+}
---
+++ MultiRoleRetail/ProductForm.cs
@@ -0,0 +1,135 @@
+﻿using MultiRoleRetail.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiRoleRetail
+{
+    public partial class ProductForm : Form
+    {
+        private readonly DatabaseRetail _db;
+        private enum Mode {None, Add, Edit}
+        private Mode _mode;
+
+        public ProductForm()
+        {
+            InitializeComponent();
+            _db = new DatabaseRetail();
+        }
+
+        private void Product_Load(object sender, EventArgs e)
+        {
+            groupBox1.Enabled = false;
+            RefreshData();
+
+            cbSupplierID.DataSource = (from p in _db.Products select p.Name).ToList();
+            cbSupplierID.SelectedItem = null;
+        }
+
+        private void RefreshData()
+        {
+            dgProduct.DataSource =
+                (
+                from pp in _db.Products
+                where pp.Name.Contains(tbName.Text) || pp.Supplier.Name.Contains(cbSupplierID.Text)
+                select new
+                {
+                    pp.ColId,
+                    pp.Name,
+                    pp.Price,
+                    pp.Stock,
+                    supplier = pp.Supplier.Name
+                }
+                ).ToList();
+        }
+
+        private void AddData()
+        {
+            var product = new Product
+            {
+                Name = tbName.Text,
+                Price = int.Parse(tbPrice.Text),
+                Stock = int.Parse(tbStock.Text),
+                Supplier = _db.Suppliers.First(sup => sup.Name == cbSupplierID.Text)
+            };
+
+            _db.Products.Add(product);
+            _db.SaveChanges();
+        }
+
+        private void EditData()
+        {
+            var prod = _db.Products.Find(tbID.Text);
+
+            prod.Name = tbName.Text;
+            prod.Price = int.Parse(tbPrice.Text);
+            prod.Stock = int.Parse(tbStock.Text);
+            prod.Supplier.Name = cbSupplierID.Text;
+
+            _db.Products.Update(prod);
+            _db.SaveChanges();
+        }
+
+        private void DeleteData(Product product)
+        {
+            _db.Remove(product);
+            _db.SaveChanges();
+        }
+
+        private bool Validation()
+        {
+            var error = string.Empty;
+
+            if (tbName.Text == string.Empty)
+            {
+                error += "Name can not be empty";
+            }
+            if (tbPrice.Text == string.Empty)
+            {
+                error += "Price can nt be empty";
+            }
+            if (tbStock.Text == string.Empty)
+            {
+                error += "At least there is 1 stock please";
+            }
+            if (cbSupplierID.SelectedItem == null)
+            {
+                error += "select a supplier";
+            }
+            else
+            {
+                MessageBox.Show(error, "Failed to add product", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            return true;
+        }
+
+        private void Clear()
+        {
+            tbID.Text = tbName.Text = tbPrice.Text = tbStock.Text = String.Empty;
+            cbSupplierID.SelectedItem = null;
+        }
+
+        private void tbSearch_TextChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            groupBox1.Enabled = true;
+            btnAdd.Enabled = btnEdit.Enabled = btnDelete.Enabled = false;
+
+            var id = _db.Products.OrderByDescending(p => p).FirstOrDefault()?.ColId[1..];
+            tbID.Text = id == null ? "P001" : $"P{(int.Parse(id) + 1):D3}";
+
+
+        }
+    }
+}
