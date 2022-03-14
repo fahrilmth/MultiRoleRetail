9968655a0c3ef856073ee64c693ce9fcc5b2df2b
Fahril <fahril16093@gmail.com>
2022-03-14 14:55:31 +0700

    Ne transaction

---
+++ MultiRoleRetail/BrowseProductForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace MultiRoleRetail
+{
+    partial class BrowseProductForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvTransaction = new System.Windows.Forms.DataGridView();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.tbSearch = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTransaction)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvTransaction
+            // 
+            this.dgvTransaction.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTransaction.Location = new System.Drawing.Point(11, 49);
+            this.dgvTransaction.Name = "dgvTransaction";
+            this.dgvTransaction.RowHeadersWidth = 62;
+            this.dgvTransaction.RowTemplate.Height = 33;
+            this.dgvTransaction.Size = new System.Drawing.Size(930, 591);
+            this.dgvTransaction.TabIndex = 0;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(829, 646);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(112, 34);
+            this.btnSelect.TabIndex = 1;
+            this.btnSelect.Text = "SELECT";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // tbSearch
+            // 
+            this.tbSearch.Location = new System.Drawing.Point(12, 12);
+            this.tbSearch.Name = "tbSearch";
+            this.tbSearch.PlaceholderText = "Search here";
+            this.tbSearch.Size = new System.Drawing.Size(929, 31);
+            this.tbSearch.TabIndex = 2;
+            this.tbSearch.TextChanged += new System.EventHandler(this.tbSearch_TextChanged);
+            // 
+            // BrowseProductForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(953, 691);
+            this.Controls.Add(this.tbSearch);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.dgvTransaction);
+            this.Name = "BrowseProductForm";
+            this.Text = "BrowseProductForm";
+            this.Load += new System.EventHandler(this.BrowseProductForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTransaction)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DataGridView dgvTransaction;
+        private Button btnSelect;
+        private TextBox tbSearch;
+    }
+}
---
+++ MultiRoleRetail/BrowseProductForm.cs
@@ -0,0 +1,53 @@
+﻿using MultiRoleRetail.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiRoleRetail
+{
+    public partial class BrowseProductForm : Form
+    {
+        private readonly DatabaseRetail _db;
+        public BrowseProductForm()
+        {
+            InitializeComponent();
+            _db = new DatabaseRetail();
+        }
+
+        private void BrowseProductForm_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void tbSearch_TextChanged(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadData()
+        {
+            dgvTransaction.DataSource =
+                (
+                from t in _db.Transactions
+                where t.User.Name.Contains(tbSearch.Text)
+                select new
+                {
+                    t.ColId,
+                    t.Date,
+                    user = t.User.Name
+                }
+                ).ToList();
+        }
+    }
+}
